f8199898dcdded69037d9ff66bdbd64e2f6f8cc0
lyqwsqk <40453234+lyqwsqk@users.noreply.github.com>
2024-03-20 15:31:34 +0800

    Add files via upload

---
+++ h5/src/pages/index/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <view>
+    <button @tap="add">add</button>
+    <view>count: {{_commonStore.count}}</view>
+    <my-picker :list="list" :on-button-click="onButtonClick"></my-picker>
+  </view>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import Picker from '../../taro/components/picker/index'
+import '../../taro/css/components/picker/index.css'
+
+import pinia from "../../stores"
+import { commonStore } from '../../stores/common'
+const _commonStore = commonStore(pinia);
+
+const MyPicker = ref(Picker)
+const add = () =>{
+  _commonStore.setCount()
+}
+</script>
